0f50ff31d871603c9a011eea50c49111c08ecab7
ulyavrubel <uvrubel@gmail.com>
2020-04-30 12:47:11 +0800

    fixed menu for mobile

--- src/Components/Navs/MobileHamburger.js
+++ src/Components/Navs/MobileHamburger.js
@@ -1,57 +1,18 @@
-import React, { useState, useContext, useRef } from "react";
-import { AuthContext } from "../Auth/AuthProvider";
-import { Link } from "@reach/router";
-import { firebaseAuth } from "../Auth/FirebaseInit";
-import useOutsideClick from "../Helpers/useOutsideClick";
+import React from "react";
 import hamburger from "../../img/hamburger.svg";
 
-function MobileHamburger() {
-  const { currentUser } = useContext(AuthContext);
-  const [open, setOpen] = useState(false);
-
-  const logout = () => {
-    firebaseAuth.auth().signOut();
-    console.log(currentUser + "signed out");
-  };
-  const ref = useRef();
-  useOutsideClick(ref, () => {
-    setOpen(false);
-  });
+function MobileHamburger(props) {
   return (
-    <div className="hamburger container mobile" ref={ref}>
+    <div className="hamburger container mobile">
       <img
         className="hamburger mobile"
         src={hamburger}
-        onClick={() => (open ? setOpen(false) : setOpen(true))}
-        style={open ? { transform: "rotate(90deg)" } : { transform: "none" }}
+        onClick={props.handleClick}
+        style={
+          props.open ? { transform: "rotate(90deg)" } : { transform: "none" }
+        }
         alt="..."
       ></img>
-      {!currentUser ? (
-        <div
-          className="hamburger links"
-          style={open ? { display: "block" } : { display: "none" }}
-          onClick={() => setOpen(false)}
-        >
-          <a href="/login">Log In</a>
-          <a href="/signup">Sign Up</a>
-        </div>
-      ) : (
-        <div
-          className="hamburger links"
-          style={open ? { display: "block" } : { display: "none" }}
-          onClick={() => setOpen(false)}
-        >
-          <Link to={`/profile/${currentUser.displayName}`}>
-            Profile: <span>{currentUser.displayName}</span>
-          </Link>
-          <Link to={`/collection/${currentUser.displayName}`}>Collection</Link>
-          <Link to={`/wishlist/${currentUser.displayName}`}>Wishlist</Link>
-          <Link to="/upload">Upload</Link>
-          <a className="logout" href="/" onClick={logout}>
-            Log Out
-          </a>
-        </div>
-      )}
     </div>
   );
 }
--- src/Components/Navs/Navbar.js
+++ src/Components/Navs/Navbar.js
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useRef } from "react";
+import { AuthContext } from "../Auth/AuthProvider";
 import { navigate } from "@reach/router";
 import logo from "../../img/logo.png";
 import logoD from "../../img/logo-MusicDB.png";
 import icon from "../../img/search-icon.svg";
 import MobileHamburger from "./MobileHamburger";
 import DesktopDropdown from "./DesktopDropdown";
+import { Link } from "@reach/router";
+import { firebaseAuth } from "../Auth/FirebaseInit";
+import useOutsideClick from "../Helpers/useOutsideClick";
 
 function Navbar() {
+  const { currentUser } = useContext(AuthContext);
   const [query, setQuery] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const logout = () => {
+    firebaseAuth.auth().signOut();
+    console.log(currentUser + "signed out");
+  };
+  const ref = useRef();
+  useOutsideClick(ref, () => {
+    setOpen(false);
+  });
 
   const handleChange = (event) => {
     event.preventDefault();
@@ -20,35 +35,78 @@ function Navbar() {
     }
   };
 
+  const handleClick = (event) => {
+    event.preventDefault();
+    setOpen(!open);
+  };
+
   return (
-    <div className="navbar">
-      <div className="logo-form">
-        <a href="/">
-          <img className="logo mobile" src={logo} alt="musicdb logo"></img>
-          <img className="logo desktop" src={logoD} alt="musicdb logo"></img>
-        </a>
-        <form className="form search">
-          <input
-            className="input search"
-            name="search"
-            id="search"
-            placeholder="Search"
-            value={query}
-            onChange={handleChange}
-            onKeyPress={handleKeyPress}
-          ></input>
-          <div
-            className="button search"
-            onClick={() => navigate(`/search/${query}/all`)}
-          >
-            <img className="img search" src={icon} alt="..."></img>
-          </div>
-        </form>
+    <div>
+      <div className="navbar">
+        <div className="logo-form">
+          <a href="/">
+            <img className="logo mobile" src={logo} alt="musicdb logo"></img>
+            <img className="logo desktop" src={logoD} alt="musicdb logo"></img>
+          </a>
+          <form className="form search">
+            <input
+              className="input search"
+              name="search"
+              id="search"
+              placeholder="Search"
+              value={query}
+              onChange={handleChange}
+              onKeyPress={handleKeyPress}
+            ></input>
+            <div
+              className="button search"
+              onClick={() => navigate(`/search/${query}/all`)}
+            >
+              <img className="img search" src={icon} alt="..."></img>
+            </div>
+          </form>
+        </div>
+
+        <MobileHamburger
+          className="mobile"
+          handleClick={handleClick}
+          open={open}
+        />
+        <DesktopDropdown className="desktop" />
       </div>
-
-      <MobileHamburger className="mobile" />
-
-      <DesktopDropdown className="desktop" />
+      <div
+        className="burger-menu mobile"
+        style={open ? { left: "0px" } : { left: "-100vw" }}
+      >
+        {!currentUser ? (
+          <div
+            className="hamburger links"
+            style={open ? { display: "block" } : { display: "none" }}
+            onClick={() => setOpen(false)}
+          >
+            <a href="/login">Log In</a>
+            <a href="/signup">Sign Up</a>
+          </div>
+        ) : (
+          <div
+            className="hamburger links mobile"
+            style={open ? { display: "block" } : { display: "none" }}
+            onClick={() => setOpen(false)}
+          >
+            <Link to={`/profile/${currentUser.displayName}`}>
+              Profile: <span>{currentUser.displayName}</span>
+            </Link>
+            <Link to={`/collection/${currentUser.displayName}`}>
+              Collection
+            </Link>
+            <Link to={`/wishlist/${currentUser.displayName}`}>Wishlist</Link>
+            <Link to="/upload">Upload</Link>
+            <a className="logout" href="/" onClick={logout}>
+              Log Out
+            </a>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
